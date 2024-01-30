649905613af09f4437a199f11ebe6d42151d4628
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-01-29 23:46:03 -0800

    initial commit

---
+++ src/main/java/com/max/vault/TheVaultApplication.java
@@ -0,0 +1,13 @@
+package com.max.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheVaultApplication.class, args);
+	}
+
+}
