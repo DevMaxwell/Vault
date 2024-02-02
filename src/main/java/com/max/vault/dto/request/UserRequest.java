7a995dbf0e36b85bc7e8773ce5f5c2e9ccdd40ce
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-02-02 14:40:52 -0800

    feat: validation for request

--- src/main/java/com/max/vault/dto/request/UserRequest.java
+++ src/main/java/com/max/vault/dto/request/UserRequest.java
@@ -1,16 +1,28 @@
 package com.max.vault.dto.request;
 
-import java.math.BigDecimal;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
+
+  @NotNull(message = "First Name cannot be null")
   private String firstName;
+  @NotNull(message = "Last name cannot be null")
   private String lastName;
   private String otherName;
+  @NotNull(message = "Gender cannot be null")
   private String gender;
+  @NotNull(message = "Address cannot be null")
   private String address;
+  @NotNull(message = "State of origin cannot be null")
   private String stateOfOrigin;
-  private String verificationNumber;
   private String email;
+  @NotNull(message = "Phone Number cannot be null")
   private String phoneNumber;
   private String alternativePhoneNumber;
   private String accountClassDescription;
