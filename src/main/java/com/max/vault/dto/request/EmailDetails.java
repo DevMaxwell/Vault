0dcd8bfe062d7b6e0283a80ac7dea8099bb28ced
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-02-13 13:47:21 -0800

    feat: send email, email dto

---
+++ src/main/java/com/max/vault/dto/request/EmailDetails.java
@@ -0,0 +1,17 @@
+package com.max.vault.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+  private String recipient;
+  private String messageBody;
+  private String subject;
+  private String attachment;
+}
