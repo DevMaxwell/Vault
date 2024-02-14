8fc54233f71822d77e95a2f24125aae101d442ee
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-02-14 12:33:45 -0800

    feat: balance enquiry

--- src/main/java/com/max/vault/repository/UserRepository.java
+++ src/main/java/com/max/vault/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.max.vault.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  Boolean existsByEmail(String email);
 
+  //User findByEmail(String email);
+  Optional<User> findByEmail(String email);
 }
