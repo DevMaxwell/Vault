0846b05e1241699d61eecd5b8da958b0b24e22f3
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-02-14 12:33:25 -0800

    feat: balance enquiry

--- src/main/java/com/max/vault/service/UserService.java
+++ src/main/java/com/max/vault/service/UserService.java
@@ -1,5 +1,6 @@
 package com.max.vault.service;
 
+import com.max.vault.dto.request.EnquiryRequest;
 import com.max.vault.dto.request.UserRequest;
 import com.max.vault.dto.response.BankResponse;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Service;
 public interface UserService {
   BankResponse createUser(UserRequest userRequest);
 
+  BankResponse balancenquiry(EnquiryRequest enquiryRequest);
+  BankResponse nameEnquiry(EnquiryRequest enquiryRequest);
 }
