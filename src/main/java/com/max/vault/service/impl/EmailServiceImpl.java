1d2a13a51dfd87ae41c01cef339853825768e82f
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-03-01 13:02:31 -0800

    feat: fine tune email service impl

--- src/main/java/com/max/vault/service/impl/EmailServiceImpl.java
+++ src/main/java/com/max/vault/service/impl/EmailServiceImpl.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
